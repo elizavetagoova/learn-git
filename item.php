5ae609fd088a935b30595a3cae7f79b2d4ca6b7b
Aslan <berezg86@gmail.com>
2017-11-22 17:20:45 +0300

    user-2

---
+++ item.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+		<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/animated-slider.css" />
+	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+	
+	
+</head>
+<body>
+branch-master
+
+
+ <!-- <a rel="group" href="<iframe width="560" height="315" src="https://www.youtube.com/embed/0QVDrCdNLJA" frameborder="0" allowfullscreen></iframe>">фэнсибокс</a> -->
+ 
+<!-- <p><a class="fancybox-media" href="http://youtu.be/GAiThZyJ1ZA?enablejsapi=1&amp;theme=light&amp;wmode=transparent&amp;showinfo=0&amp;vq=hd720&amp;border=0&amp;autoplay=1" title="Где и как скачать и установить программу Adobe Muse">Где и как скачать и установить программу Adobe Muse</a></p> -->
+
+<!-- https://youtu.be/JuGHOIIytUY -->
+	<div align="center"><a class="fancybox-media" href="https://www.youtube.com/embed/JuGHOIIytUY?version=3&enablejsapi=1&color=red&showinfo=0&vq=highres&modestbranding=1&buffering=0&loop=0&rel=0">Видео с Youtube</a></div>
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+	<script src="js/swiper.min.js"></script>
+	<script src="js/jquery.cssslider.js"></script>
+	<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+	
+	<!-- Add fancyBox main JS and CSS files -->
+	<script type="text/javascript" src="source/jquery.fancybox.js?v=2.1.5"></script>
+	<link rel="stylesheet" type="text/css" href="source/jquery.fancybox.css?v=2.1.5" media="screen" />
+
+	<!-- Add Media helper (this is optional) -->
+	<script type="text/javascript" src="source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+    <script type="text/javascript" src="source/helpers/jquery.video.js"></script>
+
+</body>
+</html>

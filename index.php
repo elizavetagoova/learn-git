29c3f1f023c4129867b8fe7ecea97d0689e9067c
Aslan <berezg86@gmail.com>
2017-11-22 15:18:47 +0300

    commit new-branch2

--- index.php
+++ index.php
@@ -13,12 +13,12 @@
 	<link rel="stylesheet" href="css/swiper.min.css">
 	<link rel="stylesheet" href="css/animated-slider.css" />
 	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
-	
+
 	
 </head>
 <body>
 
-
+hello
  <!-- <a rel="group" href="<iframe width="560" height="315" src="https://www.youtube.com/embed/0QVDrCdNLJA" frameborder="0" allowfullscreen></iframe>">фэнсибокс</a> -->
  
 <!-- <p><a class="fancybox-media" href="http://youtu.be/GAiThZyJ1ZA?enablejsapi=1&amp;theme=light&amp;wmode=transparent&amp;showinfo=0&amp;vq=hd720&amp;border=0&amp;autoplay=1" title="Где и как скачать и установить программу Adobe Muse">Где и как скачать и установить программу Adobe Muse</a></p> -->

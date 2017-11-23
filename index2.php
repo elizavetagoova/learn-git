8fc2434f0d862169dc1f5a7e3e9190302fc583e1
Aslan <berezg86@gmail.com>
2017-11-23 09:39:49 +0300

    Изменения в ветке 2

--- index2.php
+++ index2.php
@@ -30,7 +30,7 @@
 
 
 	<!-- JS_BLOCK -->
-	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
 	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
 	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
